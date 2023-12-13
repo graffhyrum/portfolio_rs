476c3699e25777fa6e515542d59f0b2857281f9e
Graff <graffhyrum@gmail.com>
2023-12-13 15:35:22 -0600

    fix project paths

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,62 @@
-use askama::Template;
 use anyhow::Context;
-use axum::{http::StatusCode, response::{Html, IntoResponse, Response}, routing::get, Router};
+use askama::Template;
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse, Response},
+    routing::get,
+    Router,
+};
 use tower_http::services::ServeDir;
 use tracing::info;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-  tracing_subscriber::registry()
-    .with(
-      tracing_subscriber::EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| "portfolio_rs=debug".into()),
-    )
-    .with(tracing_subscriber::fmt::layer())
-    .init();
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "portfolio_rs=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-  info!("Initializing Router");
+    info!("Initializing Router");
 
-  let assets_path = std::env::current_dir().unwrap();
-  let router = Router::new()
-      .route("/", get(hello))
-      .route("/contact", get(contact))
-      .route("projects", get(projects))
-      .nest_service(
-    "/assets",
-    ServeDir::new(format!("{}/assets", assets_path.to_str().unwrap())),
-  );
-  let port = 8000_u16;
-  let addr = std::net::SocketAddr::from(([0, 0, 0, 0], port));
+    let assets_path = std::env::current_dir().unwrap();
+    let router = Router::new()
+        .route("/", get(hello))
+        .route("/contact", get(contact))
+        .route("/projects", get(projects))
+        .nest_service(
+            "/assets",
+            ServeDir::new(format!("{}/assets", assets_path.to_str().unwrap())),
+        );
+    let port = 8000_u16;
+    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], port));
 
-  info!("router initialized, now listening on port {}", port);
+    info!("router initialized, now listening on addr http://{}", addr);
 
-  let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-  axum::serve(listener, router).await.context("error while starting server")?;
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, router)
+        .await
+        .context("error while starting server")?;
 
-  Ok(())
+    Ok(())
 }
 
 async fn hello() -> impl IntoResponse {
-  let template = HelloTemplate {};
-  HtmlTemplate(template)
+    let template = HelloTemplate {};
+    HtmlTemplate(template)
 }
 
 async fn contact() -> impl IntoResponse {
-  let template = ContactTemplate {};
-  HtmlTemplate(template)
+    let template = ContactTemplate {};
+    HtmlTemplate(template)
 }
 
 async fn projects() -> impl IntoResponse {
-  let template = ProjectsTemplate {};
-  HtmlTemplate(template)
+    let template = ProjectsTemplate {};
+    HtmlTemplate(template)
 }
 
 #[derive(Template)]
@@ -67,17 +74,17 @@ struct ProjectsTemplate;
 struct HtmlTemplate<T>(T);
 
 impl<T> IntoResponse for HtmlTemplate<T>
-  where
-    T: Template,
+    where
+        T: Template,
 {
-  fn into_response(self) -> Response {
-    match self.0.render() {
-      Ok(html) => Html(html).into_response(),
-      Err(err) => (
-        StatusCode::INTERNAL_SERVER_ERROR,
-        format!("Failed to render template, Error: {}", err),
-      )
-        .into_response(),
+    fn into_response(self) -> Response {
+        match self.0.render() {
+            Ok(html) => Html(html).into_response(),
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template, Error: {}", err),
+            )
+                .into_response(),
+        }
     }
-  }
 }
