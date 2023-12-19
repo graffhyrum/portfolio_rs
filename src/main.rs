ccf4e5013edc601597d056b3a4035f75aa9824c6
Josh Pendragon <pendragon.josh@gmail.com>
2023-12-18 20:21:33 -0600

    extracted router logic, added bookshelf, removed contact and replaced with an email link in the socials list, filled out projects page with gists and links.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
+mod router;
+
 use anyhow::Context;
-use askama::Template;
-use axum::{
-    http::StatusCode,
-    response::{Html, IntoResponse, Response},
-    routing::get,
-    Router,
-};
-use tower_http::services::ServeDir;
 use tracing::info;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -21,31 +15,11 @@ async fn main() -> anyhow::Result<()> {
         .init();
 
     info!("Initializing Router");
-
-    let assets_path = std::env::current_dir().unwrap();
-    let router = Router::new()
-        .route("/", get(index))
-        .route("/contact.html", get(contact))
-        .route("/projects.html", get(projects))
-        .nest_service(
-            "/assets",
-            ServeDir::new(format!("{}/assets", assets_path.to_str().unwrap())),
-        )
-        .nest_service(
-            "/styles",
-            ServeDir::new(format!("{}/styles", assets_path.to_str().unwrap())))
-        .nest_service(
-            "/scripts",
-            ServeDir::new(format!("{}/scripts", assets_path.to_str().unwrap())),
-        ).nest_service(
-        "/favicon_io",
-        ServeDir::new(format!("{}/favicon_io", assets_path.to_str().unwrap())
-        ),
-    );
     let port = 8000_u16;
     let addr = std::net::SocketAddr::from(([127, 0, 0, 1], port));
-
+    let router = router::build_router();
     info!("router initialized, now listening on addr http://{}", addr);
+
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, router)
@@ -53,49 +27,4 @@ async fn main() -> anyhow::Result<()> {
         .context("error while starting server")?;
 
     Ok(())
-}
-
-async fn index() -> impl IntoResponse {
-    let template = HelloTemplate {};
-    HtmlTemplate(template)
-}
-
-async fn contact() -> impl IntoResponse {
-    let template = ContactTemplate {};
-    HtmlTemplate(template)
-}
-
-async fn projects() -> impl IntoResponse {
-    let template = ProjectsTemplate {};
-    HtmlTemplate(template)
-}
-
-#[derive(Template)]
-#[template(path = "../templates/index.html")]
-struct HelloTemplate;
-
-#[derive(Template)]
-#[template(path = "../templates/contact.html")]
-struct ContactTemplate;
-
-#[derive(Template)]
-#[template(path = "../templates/projects.html")]
-struct ProjectsTemplate;
-
-struct HtmlTemplate<T>(T);
-
-impl<T> IntoResponse for HtmlTemplate<T>
-    where
-        T: Template,
-{
-    fn into_response(self) -> Response {
-        match self.0.render() {
-            Ok(html) => Html(html).into_response(),
-            Err(err) => (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                format!("Failed to render template, Error: {}", err),
-            )
-                .into_response(),
-        }
-    }
 }
