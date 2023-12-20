730b7006639832049a516e1ccb946c68348879f3
Josh Pendragon <pendragon.josh@gmail.com>
2023-12-20 16:15:05 -0600

    implement fallback and extract router components

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -4,6 +4,12 @@ use axum::{Router, routing::get};
 use tower_http::services::ServeDir;
 use crate::router::handlers::{bookshelf::bookshelf, index::index, my_work::my_work, testimonials::testimonial};
 use anyhow::Result;
+use axum::http::StatusCode;
+
+async fn fallback() -> (StatusCode, &'static str) {
+    (StatusCode::NOT_FOUND, "Not Found")
+}
+
 
 pub fn build_router() -> Result<Router> {
     let assets_path_buff = std::env::current_dir()?;
@@ -11,13 +17,23 @@ pub fn build_router() -> Result<Router> {
         .to_str()
         .ok_or(anyhow::anyhow!("Failed to convert path to string"))?;
 
-    Ok(Router::new()
+    let routes = Router::new()
         .route("/", get(index))
         .route("/my_work/my_work.html", get(my_work))
         .route("/bookshelf/bookshelf.html", get(bookshelf))
         .route("/testimonials/testimonials.html", get(testimonial))
+        .fallback(fallback);
+
+    let services = Router::new()
         .nest_service("/assets", ServeDir::new(format!("{}/assets", assets_path)))
         .nest_service("/styles", ServeDir::new(format!("{}/styles", assets_path)))
         .nest_service("/scripts", ServeDir::new(format!("{}/scripts", assets_path)))
-        .nest_service("/favicon_io", ServeDir::new(format!("{}/favicon_io", assets_path))))
+        .nest_service("/favicon_io", ServeDir::new(format!("{}/favicon_io", assets_path)));
+
+    let app = Router::new()
+        .merge(routes)
+        .merge(services);
+
+
+    Ok(app)
 }
